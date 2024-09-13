db8712f8e6cf0051255d0b37783f9e3bbc2980d3
sm <sahm1383@gmail.com>
2024-09-13 22:45:58 +0330

    1

--- django_news/settings.py
+++ django_news/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f=$h*b==ndx-xmtp9ilu67)v+n5yydlcnjw4h#za3wa%%kad=x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
